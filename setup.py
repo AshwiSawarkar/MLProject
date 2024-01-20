6ccf6f10a8e9d4efe57e0470cbdb5d925296289c
Ashwini Sawarkar <ashwi1987@gmail.com>
2024-01-20 10:47:52 +0000

    Build Packahe

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages,setup
 from typing import List
+
+HYPEN_E_DOT='-e.'
+
 def get_requirements(file_path:str)->List[str]:
     '''
     This function will return list of requirenments for the project like packages and all 
@@ -7,11 +10,14 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.rplace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
+    return requirements
 
 setup(
-name="Mlroject",
+name="MlProject",
 version='0.0.1',
 author='AshwiniSSawarkar',
 author_email='ashwi1987@gmail.com',
